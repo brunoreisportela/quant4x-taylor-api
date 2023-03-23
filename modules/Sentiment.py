4ad91b39784da3fa1c169eaa663f9ae5c045ef82
Bruno Portela <breisportela@gmail.com>
2023-03-23 16:17:38 -0400

    Update

--- modules/Sentiment.py
+++ modules/Sentiment.py
@@ -55,7 +55,7 @@ class Sentiment:
                 symbol  = symbol[u'symbol'],
                 screener= symbol[u'screener'],
                 exchange= symbol[u'exchange'],
-                interval= Interval.INTERVAL_4_HOURS,
+                interval= Interval.INTERVAL_1_DAY,
                 # proxies={'http': 'http://example.com:8080'} # Uncomment to enable proxy (replace the URL).
             )
 
