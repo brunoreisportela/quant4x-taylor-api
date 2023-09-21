f32c9295666b3b633f4918af08bbf7a3dbcd243d
Bruno Portela <breisportela@gmail.com>
2023-09-21 08:19:26 -0400

    update

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def get_account_setup():
     return json.dumps(dbController.get_account_setup(account_id))
 
 @app.route("/account/setup/set_start_balance", methods=['GET'])
-def get_account_setup():
+def set_start_balance():
     account_id = request.args["account_id"]
     balance = request.args["balance"]
 
