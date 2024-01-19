29c6c937710c35fbd53849555a1bef5231e37ae8
Bruno Portela <breisportela@gmail.com>
2024-01-19 01:04:31 -0500

    Added the payload read

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def add_performance():
     if token not in valid_tokens:
         return {"error": "Invalid token"}, 401
 
-    payload = request.form["payload"]
+    payload = request.form.get["payload"]
     return dbController.add_performance(payload)
 
 @app.route("/daily/performance", methods=['POST'])
