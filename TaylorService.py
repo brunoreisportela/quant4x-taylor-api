3b64fe37f631bc6c6163d70b483e214479d85f34
Bruno Portela <breisportela@gmail.com>
2024-04-02 15:30:08 -0400

    Update

--- TaylorService.py
+++ TaylorService.py
@@ -22,23 +22,18 @@ class TaylorService:
     db_controller = DBController()
     db_controller_modules = DBControllerModules()
 
-    api_base_URL = "https://taylor.ngrok.app"
-    cloudbet_URL = "https://sports-api.cloudbet.com/pub/v2"
-
-    cloudbet_key = "eyJhbGciOiJSUzI1NiIsImtpZCI6IkhKcDkyNnF3ZXBjNnF3LU9rMk4zV05pXzBrRFd6cEdwTzAxNlRJUjdRWDAiLCJ0eXAiOiJKV1QifQ.eyJhY2Nlc3NfdGllciI6InRyYWRpbmciLCJleHAiOjIwMTc2Nzc4NDYsImlhdCI6MTcwMjMxNzg0NiwianRpIjoiYTU5NGVmMzQtNWQ0My00OWZkLWFlY2UtNjVkOWM3NzlkOGRmIiwic3ViIjoiN2QxMDBjY2UtNWU0Ni00ZjI0LWJiNTEtYWVkNWQ1ODQ2YjAwIiwidGVuYW50IjoiY2xvdWRiZXQiLCJ1dWlkIjoiN2QxMDBjY2UtNWU0Ni00ZjI0LWJiNTEtYWVkNWQ1ODQ2YjAwIn0.aON3xNrNxUix8F7wFhg85ZtnaU0PUsW4rk3LKhCHJEK4CD6i9FXomt80QsRAF2TJAmRnEdcK0drYVVv61XqwSjwcYM3AvuUwfmakysI8VBH5FEiLWLXQLxo6Y1Jns9XrnGKECNIaqG5ejQm_f1KLUDwVTxMSyk84aFgNPLQob5Q-obUyaWB9ZAq4IT-N9HiZzvyMulYRG7t2YIaGacZOvkukvaZGKH8pMMY8IzxfK1TIwStSs2NkS7WmEuHe-x9_YUJHMOPpZ7bflIYW35iXt00e6TuB7UBEs6oad2JgRx8urVMqANw1_zlz8zJmNIZUWr7Q7UARYVG35ECAeSSbMQ"
-
-    def schedule_task(self, event_name):
+    def schedule_task(self, symbol_name):
         task_id = str(uuid.uuid4())
         print(f"Queue added to the pipeline: {task_id}")
 
         task = Task
         task["id"] = task_id
-        task["processor"] = ProcessorType.PROMPT_BET_TENNIS_CREW.value
+        task["processor"] = ProcessorType.PROMPT_INVESTMENT_CREW.value
         task["status"] = StatusType.PENDING.value
-        task["prompt"] = event_name
+        task["prompt"] = symbol_name
         task["webhook"] = ""
 
-        self.db_controller_modules.insert_task(task)
+        return self.db_controller.insert_task(task)
 
     def get_task_id(self, event):
         headers = {
@@ -121,88 +116,73 @@ class TaylorService:
                         self.db_controller.update_event(event, "no_relation", "undefined", "INVALID_JSON")
         
         return waiting_tasks
-
-    def normalize_event(self, event):
-        sport_name = "tennis"
-
-        if "players" in event and "markets" in event["players"][0] and "markets" in event["players"][1]:
-            for player in event["players"]:
-                for market in player["markets"]:
-                    if market["type"] == f"{sport_name}.winner":
-                        player["american_odd"] = ConvertionUtils.convert_to_american_odds(player["price"])
-                        player["market_type"] = market["type"]
-                        player["probability"] = market["probability"]
-                        player["max_stake"] = market["maxStake"]
-
-                    if market["type"] == f"{sport_name}.winner_of_set":
-                        event["current_set"] = market["params"]
-
-            event["american_odd_distance"] = ConvertionUtils.compare_distances(event["players"][0]["american_odd"], event["players"][1]["american_odd"])
-
-            event["max_stake"] = event["players"][0]["max_stake"] if event["players"][0]["max_stake"] > event["players"][1]["max_stake"] else event["players"][1]["max_stake"]
-
-            event["home_american_odd"] = event["players"][0]["american_odd"]
-            event["away_american_odd"] = event["players"][1]["american_odd"]
+    
+    def sync_symbols_to_predict(self):
         
-        event = ConvertionUtils.convert_camel_to_snake(event)
-        return event
-        
-    def sync_events_to_predict(self):
-        
-        headers = {
-            "Content-Type": "application/json",
-            "X-API-Key": self.cloudbet_key,
-        }
-        
-        sport_name = "tennis"
+        list_to_predict = ["GBPUSD", "EURUSD", "USDJPY", "USDCAD", "GOLD", "SP500"]
 
-        start_epoch, end_epoch = ConvertionUtils.get_day_epoch_times()
-
-        response = requests.get(f"{self.cloudbet_URL}/odds/events?sport={sport_name}&from={start_epoch}&to={end_epoch}&live=false&limit=10000", headers=headers, timeout=120)
-        
-        print(f"{Fore.GREEN}Event Request Status: {response.status_code} - {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-
-        events = []
-
-        if response.status_code == 200:
+        for symbol in list_to_predict:
             
-            if "events" in response.json() == False:
-                return events
+            cursor_count = self.schedule_task(symbol)
+
+            if cursor_count > 0:
+                print(f"{Fore.LIGHTGREEN_EX}Symbol {symbol} inserted")
+            else:
+                print(f"{Fore.LIGHTYELLOW_EX}Warning: Symbol {symbol} not inserted")
+
+        # headers = {
+        #     "Content-Type": "application/json",
+        #     "X-API-Key": self.cloudbet_key,
+        # }
+        
+        # sport_name = "tennis"
+
+        # start_epoch, end_epoch = ConvertionUtils.get_day_epoch_times()
+
+        # response = requests.get(f"{self.cloudbet_URL}/odds/events?sport={sport_name}&from={start_epoch}&to={end_epoch}&live=false&limit=10000", headers=headers, timeout=120)
+        
+        # print(f"{Fore.GREEN}Event Request Status: {response.status_code} - {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+
+        # events = []
+
+        # if response.status_code == 200:
             
-            json_competitions = response.json()["competitions"]
+        #     if "events" in response.json() == False:
+        #         return events
+            
+        #     json_competitions = response.json()["competitions"]
 
-            for competition in json_competitions:
-                json_events = competition["events"]
+        #     for competition in json_competitions:
+        #         json_events = competition["events"]
 
-                print(f"{Fore.GREEN}Events: {Fore.LIGHTYELLOW_EX}{len(json_events)} - {Fore.GREEN}{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+        #         print(f"{Fore.GREEN}Events: {Fore.LIGHTYELLOW_EX}{len(json_events)} - {Fore.GREEN}{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
-                for event in json_events:
-                    event = self.normalize_event(event)
+        #         for event in json_events:
+        #             event = self.normalize_event(event)
 
-                    event_to_predict = {}
-                    event_to_predict["id"] = event["id"]
-                    event_to_predict["name"] = event["name"]
-                    event_to_predict["player_home_name"] = event["home"]["name"]
-                    event_to_predict["player_away_name"] = event["away"]["name"]
+        #             event_to_predict = {}
+        #             event_to_predict["id"] = event["id"]
+        #             event_to_predict["name"] = event["name"]
+        #             event_to_predict["player_home_name"] = event["home"]["name"]
+        #             event_to_predict["player_away_name"] = event["away"]["name"]
 
-                    cursor_count = self.db_controller.insert_event(event_to_predict)
+        #             cursor_count = self.db_controller.insert_event(event_to_predict)
 
-                    if cursor_count > 0:
-                        self.schedule_task(event_to_predict["name"])
-                    else:
-                        print(f"{Fore.LIGHTYELLOW_EX}Warning: Event not inserted")
+        #             if cursor_count > 0:
+        #                 self.schedule_task(event_to_predict["name"])
+        #             else:
+        #                 print(f"{Fore.LIGHTYELLOW_EX}Warning: Event not inserted")
 
-        else:
-            print(f"{Fore.LIGHTRED_EX}Error {response.status_code}: {response.text}")
+        # else:
+        #     print(f"{Fore.LIGHTRED_EX}Error {response.status_code}: {response.text}")
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        self.sync_events_to_predict()
+        self.sync_symbols_to_predict()
         time.sleep(1)
         self.load_waiting_tasks()
-        time.sleep(1)        
-        # self.load_pending_events(tasks)
+        time.sleep(1)
 
 if __name__ == "__main__":
     print(Fore.LIGHTYELLOW_EX+"Application Opened")
